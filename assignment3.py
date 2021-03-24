e7ef6f3dc99e5cc87d7cd7a8e141e18e06c7c9bb
MatthewTalbot <43119203+MatthewTalbot@users.noreply.github.com>
2021-03-24 16:24:15 -0400

    initialized variables

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,13 @@
-class t_qlearning:
+import pandas as pd
+import numpy as np
+class td_qlearning:
   def __init__(self, path):
     #setup
-    pass
+    self.learning_rate = 0.1
+    self.discount_factor = 0.5
+    self.reward = -1
+    self.trajectory = pd.read_csv(path, header=None)
+    
   
   def qvalue(self, state, action):
     #setup
@@ -10,3 +16,11 @@ class t_qlearning:
   def policy(self, state):
     #setup
     pass
+
+#testing
+def main():
+  blah = td_qlearning("C:\\Users\\Matthew Talbot\\OneDrive\\Documents\\AI\\QLearning\\Example1\\Example1\\trajectory.csv")
+  print(blah.trajectory[0])
+
+if __name__ == "__main__":
+  main()
